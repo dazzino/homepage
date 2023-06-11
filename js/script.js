8b55ad5e174c89b735825ee56bfc466816544f12
dazzino <136257650+dazzino@users.noreply.github.com>
2023-06-12 01:18:05 +0900

    첫번째 업로드

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// You may use JavaScript for handling interactions on your website, like clicking on a button to open a modal, submitting a form, or implementing a carousel for reviews.
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Your JavaScript code goes here
+})
